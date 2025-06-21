4d1a85ae4014fd597e0b2d3a5a2165b724bcde38
Saurav Kumar <sauravkumar@Sauravs-MacBook-Air.local>
2025-06-21 13:27:10 +0530

    Initial Base setup

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import MachineCoding from "./views/MachineCoding";
+
+function App() {
+  return <MachineCoding />;
+}
+
+export default App;
---
+++ src/views/MachineCoding/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MachineCoding = () => {
+  return (
+    <div>
+      <h1>Machine Coding</h1>
+    </div>
+  );
+};
+
+export default MachineCoding;
