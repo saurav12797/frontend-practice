8b0e0dae078ae49a49aa7e39075b97e85916bafa
Saurav Kumar <sauravkumar@Sauravs-MacBook-Air.local>
2025-06-22 11:09:43 +0530

    Add Counter

---
+++ src/views/MachineCoding/Counter/index.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export default function CounterTimer() {
+  const [countValue, setCountValue] = useState(0);
+
+  const handleCounter = (type) => {
+    switch (type) {
+      case "increment": {
+        setCountValue((prev) => prev + 1);
+        break;
+      }
+      case "reset": {
+        setCountValue(0);
+        break;
+      }
+      case "decrement": {
+        if (countValue > 0) setCountValue((prev) => prev - 1);
+        break;
+      }
+      case "default":
+        return;
+    }
+  };
+
+  return (
+    <div>
+      <h2>{countValue}</h2>
+      <button onClick={() => handleCounter("decrement")}>Decrement</button>
+      <button onClick={() => handleCounter("reset")}>Reset</button>
+      <button onClick={() => handleCounter("increment")}>Increment</button>
+    </div>
+  );
+}
